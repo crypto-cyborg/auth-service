0e1e1069a1df2db5bd26c276fb7e698b417547cd
tng <m.farm4nov@gmail.com>
2024-09-21 16:15:12 +0400

    forked from Academy/AuthService

---
+++ AuthService/Extensions/UserFactory.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Models;
+
+namespace AuthService.Extensions
+{
+    public static class UserFactory
+    {
+        public static User Create(Guid userId,
+            string username, string passwordHash,
+            string email, string name, string surname)
+        {
+            return new User
+            {
+                Id = userId,
+                Username = username,
+                PasswordHash = passwordHash,
+                Email = email,
+                FirstName = name,
+                LastName = surname
+            };
+        }
+    }
+
+}
---
+++ AuthService/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using DataLayer.Models;
+using System.Security.Claims;
+
+namespace AuthService.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> Generate(User user);
+        string GenerateRandomToken();
+        Task<ClaimsIdentity> GetPrincipalFromExpiredToken(string token);
+    }
+}
